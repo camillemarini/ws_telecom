1af2aeea78fcdeb859f8d37554bfce414c2c6947
Camille <camille.marini@feetme.fr>
2016-03-20 23:40:31 +0100

    votap needs user to be logged in

--- frontapp/urls.py
+++ frontapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^registration$', views.registration, name='registration'),
     url(r'^registration_ok$', views.registration_ok, name='registration_ok'),
     url(r'^registration_admin$', views.registration_admin, name='admin'),
+    url(r'^vote_up_down_probicipant/(?P<probicipant_id>\d+)/$',
+        views.vote_up_down_probicipant, name='vote_up_down_probicipant'),
 ]
